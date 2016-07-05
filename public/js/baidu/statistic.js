204fadc36193f597c622bac713ebfea1968357e2
robin.zhang <robin.zhang@timanetworks.com>
2016-07-05 21:52:27 +0800

    add baidu analysis code

---
+++ public/js/baidu/statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+	var hm = document.createElement("script");
+	hm.src = "//hm.baidu.com/hm.js?0c1a08241aaddb36f98854d01d7d7ccc";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
+})();
