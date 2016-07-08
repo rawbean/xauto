8d48029ba842d85fe44eb024804042bf46c6464a
robin.zhang <robin.zhang@timanetworks.com>
2016-07-08 12:49:03 +0800

    add theme

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -74,6 +74,7 @@ exports = module.exports = function (req, res) {
 
 		q.exec(function (err, results) {
 			locals.data.posts = results;
+			console.log(JSON.stringify(results))
 			next(err);
 		});
 	});
